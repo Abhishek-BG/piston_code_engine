1456a1ec29c5c3fff72c5df13d3b7be7d6fdd89a
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2024-08-13 21:50:07 +0530

    bug fixes

--- piston_backend/index.js
+++ piston_backend/index.js
@@ -14,9 +14,11 @@ app.post('/run-code', async (req, res) => {
     let results = [];
     for (const input of inputs) {
         try {
-            const result = await piston.execute(language, code, { stdin: input });
+            const result =(language==="cpp")? await piston.execute('cpp', code, { language: 'cpp', version: '10.2.0', stdin: input }) : await piston.execute(language, code, { stdin: input });
+            console.log(result);
             results.push(result.run.output);
         } catch (error) {
+            console.error(error);
             results.push(`${error.message || 'Error executing'}`);
         }
     }
@@ -33,7 +35,7 @@ app.post('/submit-code', async (req, res) => {
     let expectedOutputs = [];
     for (const input of inputs) {
         try {
-            const correctResult = await piston.execute(correctLanguage, correctCode, { stdin: input });
+            const correctResult = correctLanguage!=="cpp"? await piston.execute(correctLanguage, correctCode, { stdin: input }) : await piston.execute('cpp', correctCode, { language: 'cpp', version: '10.2.0', stdin: input });
             expectedOutputs.push(correctResult.run.output || 'No output');
         } catch (error) {
             expectedOutputs.push(`Error: ${error.message || 'Error executing correct code'}`);
@@ -44,7 +46,7 @@ app.post('/submit-code', async (req, res) => {
     for (let i = 0; i < inputs.length; i++) {
         const input = inputs[i];
         try {
-            const result = await piston.execute(language, code, { stdin: input });
+            const result =(language==="cpp")? await piston.execute('cpp', code, { language: 'cpp', version: '10.2.0', stdin: input }) : await piston.execute(language, code, { stdin: input });
             const userOutput = result.run.output || 'No output';
             const expectedOutput = expectedOutputs[i];
 
